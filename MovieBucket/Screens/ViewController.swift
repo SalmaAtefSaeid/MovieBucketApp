c3fd9bdfef0f88bcdb1502d6d458a388aa3d2f2f
Nouran Amr <nouranamr206@gmail.com>
2019-05-13 20:46:44 +0200

    updated desgin

--- MovieBucket/Screens/ViewController.swift
+++ MovieBucket/Screens/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    
+    @IBOutlet var choicesCollection: [UIButton]!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sortBtn(_ sender: UIButton) {
+        choicesCollection.forEach { (button) in
+            UIView.animate(withDuration: 0.3, animations:{
+                button.isHidden = !button.isHidden
+                self.view.layoutIfNeeded()
 
+            })
+                    }
+    }
+    enum choices  :  String
+    {
+        case mostPopular = "mostPopular"
+        case HighestRate = "rate"
+    }
+    
+    @IBAction func selectedChoices(_ sender: UIButton) {
+        guard let title = sender.currentTitle , let menuChoice = choices(rawValue:title)else{
+            return
+        }
+        switch menuChoice {
+            
+        case .mostPopular :
+            print("populaaaar")
+        default:
+            print("highestRate")
+        }
+    }
+  
+    
+   
 }
 
